d8d4e1abe9ee9c03290ac97b81fb5a179bf4a36e
ale-cia <ale-cia@users.noreply.github.com>
2017-06-23 14:54:09 -0500

    Added jQuery Peer Challenges

---
+++ jQuery_peer_mini_challenges/challenges/chal1.js
@@ -0,0 +1,14 @@
+/*
+  Add a DIV to the Container Class.
+  Inside the DIV, add a P tag that has your name
+  Then add another P tag with the City you live in.
+*/
+
+$(document).ready(function(){
+ appendDom();
+});
+
+function appendDom(){
+  //Write Append Dom Code Here
+  $(".container").append("<p>A & A</p>", "<p>Minneapolis</p>");
+}
---
+++ jQuery_peer_mini_challenges/challenges/chal2.js
@@ -0,0 +1,20 @@
+/*
+    Add an event listener to the Button that is created with the
+    appendDom, that when you click the button, 'meow' is console.logged.
+*/
+
+$(document).ready(function(){
+
+  appendDom();
+
+});
+
+function appendDom(){
+  $(".container").append("<div></div>");
+  var $el = $(".container").children().last();
+  $el.append("<button>Meow</button>");
+
+  $("button").on("click", function(){
+    console.log("meow");
+  });
+}
